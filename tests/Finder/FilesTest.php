c071504fb9e391aa2bf04421e1ee4c00d3c6a8c7
Thomas Weinert <thomas@weinert.info>
2018-09-07 15:07:13 +0200

    Add files finder iterator

---
+++ tests/Finder/FilesTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of php-file-iterator.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SebastianBergmann\FileIterator\Finder;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \SebastianBergmann\FileIterator\Finder\Files
+ * @uses \SebastianBergmann\FileIterator\Filter\SubString
+ */
+final class FilesTest extends TestCase
+{
+
+    public function testFilesWithNonExistingPathReturnsIteratorWithoutElements():void
+    {
+        $files = new Files(__DIR__.'/NonExistingDirectory');
+        $this->assertCount(0, $files);
+    }
+
+    public function testFilesReturningFileInFixtureDirectory():void
+    {
+        $files = new Files(__DIR__.'/_fixtures');
+        $this->assertCount(1, $files);
+    }
+
+    public function testFilesWithMultipleDirectoriesReturningFileInFixtureDirectory():void
+    {
+        $files = new Files([__DIR__.'/_fixtures', __DIR__.'/NonExistingDirectory']);
+        $this->assertCount(1, $files);
+    }
+
+
+    public function testFilesWithWildcardDirectoryReturningFileInFixtureDirectory():void
+    {
+        $files = new Files([__DIR__.'/_fixtures/File*']);
+        $this->assertCount(1, $files);
+    }
+
+
+    public function testFilesReturningFileInFixtureDirectoryWithPrefixAndSuffix():void
+    {
+        $files = new Files(__DIR__.'/_fixtures', 'dummy', '.txt');
+        $this->assertCount(1, $files);
+    }
+
+    public function testFilesFilterFileWithPrefix():void
+    {
+        $files = new Files(__DIR__.'/_fixtures', 'invalid');
+        $this->assertCount(0, $files);
+    }
+
+    public function testFilesFilterFileWithSuffix():void
+    {
+        $files = new Files(__DIR__.'/_fixtures', '', 'invalid');
+        $this->assertCount(0, $files);
+    }
+}
