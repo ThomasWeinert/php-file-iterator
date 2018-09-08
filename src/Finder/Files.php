a9a4707e825c238703747766de431f22d796bb09
Thomas Weinert <thomas@weinert.info>
2018-09-08 09:50:31 +0200

    Fix: refactor files finder into an Iterator

--- src/Finder/Files.php
+++ src/Finder/Files.php
@@ -14,7 +14,7 @@ namespace SebastianBergmann\FileIterator\Finder;
 use SebastianBergmann\FileIterator\Filter\SubString as SubStringFilter;
 use SebastianBergmann\FileIterator\WildcardPaths;
 
-final class Files implements \IteratorAggregate
+final class Files implements \OuterIterator
 {
     /**
      * @var array
@@ -28,6 +28,10 @@ final class Files implements \IteratorAggregate
      * @var array|string
      */
     private $suffixes;
+    /**
+     * @var \Iterator
+     */
+    private $innerIterator;
 
     /**
      * @param array|string $paths
@@ -44,9 +48,13 @@ final class Files implements \IteratorAggregate
         $this->suffixes = $suffixes;
     }
 
-    public function getIterator(): \Iterator
+    public function getInnerIterator(): \Iterator
     {
-        $filteredFiles = new SubStringFilter(
+        if (null !== $this->innerIterator) {
+            return $this->innerIterator;
+        }
+
+        $this->innerIterator = new SubStringFilter(
             new SubStringFilter(
                 $files = new \AppendIterator(),
                 $this->suffixes,
@@ -70,6 +78,31 @@ final class Files implements \IteratorAggregate
             );
         }
 
-        return $filteredFiles;
+        return $this->innerIterator;
+    }
+
+    public function current()
+    {
+        return $this->getInnerIterator()->current();
+    }
+
+    public function next(): void
+    {
+        $this->getInnerIterator()->next();
+    }
+
+    public function key()
+    {
+        return $this->getInnerIterator()->key();
+    }
+
+    public function valid(): bool
+    {
+        return $this->getInnerIterator()->valid();
+    }
+
+    public function rewind(): void
+    {
+        $this->getInnerIterator()->rewind();
     }
 }
